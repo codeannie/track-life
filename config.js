350a741a99ebdbaff0fd667d9245faeb44715f8b
Annie Chiang <devbear22@gmail.com>
2017-12-04 20:00:15 -0800

    remove demo user info -- revert back to original setting

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 const DATABASE_URL = process.env.DATABASE_URL || 
                     global.DATABASE_URL ||
-                    'mongodb://localhost/track-life-test2';
+                    'mongodb://localhost/track-life';
 
 const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                          'mongodb://localhost/test-track-life2';
+                          'mongodb://localhost/test-track-life';
 
 const PORT = process.env.PORT || 8080;
 const JWT_SECRET = 'trackyolife';
 const JWT_EXPIRY = process.env.JWT_EXPIRY || '3d';
 
-const demoName =  process.env.demoName;
-const demoPassword = process.env.demoPassword;
-
-
-module.exports = {DATABASE_URL, JWT_EXPIRY, JWT_SECRET, PORT, TEST_DATABASE_URL, demoName, demoPassword};
+module.exports = {DATABASE_URL, JWT_EXPIRY, JWT_SECRET, PORT, TEST_DATABASE_URL};
